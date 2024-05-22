ba900938fbd5ae644f5af444b421eca36299f521
Yash vardhan <devyash0308@gmail.com>
2024-05-22 18:12:26 +0530

    in progress

--- E-Commerce/frontend/src/Pages/Product.jsx
+++ E-Commerce/frontend/src/Pages/Product.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import { Breadcrum } from '../Components/Breadcrums/Breadcrum'
-
+import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay'
 export const Product = () => {
   const { all_product } = useContext(ShopContext)
   const { productId } = useParams()
@@ -10,6 +10,7 @@ export const Product = () => {
   return (
     <div>
       <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
